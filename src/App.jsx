24fc7c69cdd27bbdcbfc1987ff6d49d1d3f2d029
prajwal <prajwalkotagi2002@gmail.com>
2024-02-15 13:16:12 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Adddata from './assets/Components/Field/Adddata'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tudo List</h1>
+      <Adddata />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Field/Adddata.jsx
@@ -0,0 +1,33 @@
+import { Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { DeleteOutlined, PlusOutlined} from '@ant-design/icons'
+
+const Adddata = () => {
+    const [data, setdata] = useState("");
+    const [list, setlist] = useState([]);
+    function handledata(){
+        setlist([...list, data])
+        console.log(list)
+    }
+    function removehandle(){
+        setdata(list.pop())
+    }
+
+    return (
+        <>
+            <div>
+                <TextField id="outlined-basic" label="Enter Data" variant="outlined" name='data' value={data} onChange={(e)=> setdata(e.target.value)} size='sm'   style={{width: '600px'}}/>
+                <Button variant="outlined" style={{width: '200px', height: '55px'}} onClick={handledata} >Submit</Button> 
+            </div>
+            <div style={{width: '800px', height:'400px', overflow: 'scroll'}}>
+                {
+                    list!= 0 && list.map((demo, i)=>{
+                        return <p key={i} style={{width: '700px', backgroundColor: 'yellow', textAlign: 'start', padding: '20px'}}  >{demo}<DeleteOutlined style={{float: 'right'}} onClick={removehandle}/></p>
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default Adddata
